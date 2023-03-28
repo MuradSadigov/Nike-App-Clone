b7bd3e56a6bbeee04c216c9c5f93340ff3c4bff2
muradsadigov <sadigov@digitale-lernumgebung.de>
2023-03-28 23:52:40 +0200

    Products Detail Page - Done

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { ProductsScreen } from "./src/screens/ProductsScreen";
+import { ProductDetailScreen } from "./src/screens/ProductDetailScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <ProductsScreen /> */}
+      <ProductDetailScreen />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+
   },
 });
---
+++ src/screens/ProductDetailScreen.js
@@ -0,0 +1,79 @@
+import {
+  View,
+  StyleSheet,
+  Image,
+  FlatList,
+  ScrollView,
+  Text,
+  useWindowDimensions,
+  Pressable,
+} from "react-native";
+import products from "../data/products";
+
+export const ProductDetailScreen = () => {
+  const product = products[0];
+  const { width } = useWindowDimensions();
+
+  const addToCart = () => {
+  }
+
+  return (
+    <View>
+      <ScrollView>
+        <FlatList
+          data={product.images}
+          renderItem={({ item }) => (
+            <Image source={{ uri: item }} style={{ width, aspectRatio: 1 }} />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+        />
+
+        <View style={{ padding: 20 }}>
+          <Text style={styles.title}>{product.name}</Text>
+          <Text style={styles.price}>${product.price}</Text>
+          <Text style={styles.description}>{product.description}</Text>
+        </View>
+      </ScrollView>
+
+      <Pressable onPress={addToCart} style={styles.button}>
+        <Text style={styles.buttonText}>Add to Chart</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 34,
+    fontWeight: "500",
+    marginVertical: 10,
+  },
+  price: {
+    fontSize: 16,
+    fontWeight: "500",
+    letterSpacing: 1.5,
+  },
+  description: {
+    fontSize: 18,
+    lineHeight: 30,
+    fontWeight: "300",
+    marginVertical: 10,
+  },
+  button: {
+    position: "absolute",
+    backgroundColor: "black",
+    bottom: 30,
+    width: "90%",
+    alignSelf: "center",
+    padding: 20,
+    borderRadius: 50,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+});
